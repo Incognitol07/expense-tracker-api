01b8d220b621c202bedbe10b93407b2ce81f95c3
AdelodunAbraham <ab.adelodun@gmail.com>
2024-11-10 09:53:56 +0100

    chore(schemas) added AdminCreate schema

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,6 +1,6 @@
 # app/schemas/__init__.py
 
-from .auth import UserCreate, UserLogin, UserResponse  # Auth-related schemas
+from .auth import UserCreate, UserLogin, UserResponse, AdminCreate  # Auth-related schemas
 from .expenses import ExpenseCreate, ExpenseResponse  # Expense-related schemas
 from .categories import CategoryCreate, CategoryResponse  # Category-related schemas
 from .budget import BudgetCreate, BudgetResponse  # Budget-related schemas
